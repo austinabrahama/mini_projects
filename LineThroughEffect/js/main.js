c57560c8bfe5cff736ae5b82a3f8c692b0428c38
Austin Abraham <austin-9903@csez.zohocorpin.com>
2023-02-19 15:02:33 +0530

    Line Through Effect

---
+++ LineThroughEffect/js/main.js
@@ -0,0 +1,17 @@
+
+var textEle = document.getElementsByTagName("h1")[0];
+var text = textEle.innerHTML;
+var textArray = text.split("");
+
+var newEle = document.createElement("h1");
+newEle.classList.add("overlay");
+
+newEle.innerHTML = textArray.map(value => {
+    return `<span style='${showRandomly()}'>${value}</span>`;
+}).join("");
+document.body.appendChild(newEle);
+
+
+function showRandomly() {
+    return (Math.random() < 0.5) ? 'visibility: hidden;' : 'visibility: visible;';
+}
