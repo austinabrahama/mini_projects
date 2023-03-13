e84dcf289e4c500f7418509315ed2770f9e37da9
Austin Abraham <austin-9903@csez.zohocorpin.com>
2023-03-14 00:04:06 +0530

    Auto Text Effect

---
+++ AutoTextEffect/js/main.js
@@ -0,0 +1,15 @@
+var content = document.getElementById("content");
+var text = "We love programming";
+var textLength = text.length;
+var count = 1;
+
+function contentEffect() {
+    content.innerHTML = text.substring(0, count);
+    count++;
+
+    if (count == textLength + 1) {
+        count = 1;
+    }
+}
+
+setInterval(contentEffect, 1000);
